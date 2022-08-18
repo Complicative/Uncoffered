074576c8c1d16adbf270915131cdf2495910809e
Complicative <mark.kleymann@outlook.com>
2022-08-18 06:46:37 +0200

    Initial commit

---
+++ Uncoffered/Uncoffered.lua
@@ -0,0 +1,369 @@
+Uncoffered = {
+  name = "Uncoffered",
+  version = "1.0.0",
+  author = "@Complicative",
+}
+
+Uncoffered.Settings = {
+  --Saved Settings
+  --None for now
+}
+
+local LAM2 = LibAddonMenu2
+local debug = false
+
+--------------------------------------
+
+--Local Utility Functions
+local function cStart(hex) return "|c" .. hex end --returns colour start for a string
+
+local function cEnd() return "|r" end --return colour end string
+
+local function getTimeStamp() return cStart("888888") .. "[" .. os.date('%H:%M:%S') .. "] " .. cEnd() end --returns a timestamp in gray
+
+local pColoredStr = function(num1, num2, c1, c2) --returns a percantage of num1 in colour c1 if num1 > num2. c2 if num1 < num2
+  local color = function() if num1 > num2 then return c1 else return c2 end end
+  return string.format("%s%.2f%s%s", cStart(color()), num1 * 100, "%", cEnd())
+end
+
+------------------------------------
+
+
+function Uncoffered.IsCollectedFromSetId(setId, i)
+  --returns if item is collected
+  local _, slot = GetItemSetCollectionPieceInfo(setId, i)
+  return IsItemSetCollectionSlotUnlocked(setId, slot)
+end
+
+function Uncoffered.GetItemLinkFromId(id) local cofferId = id
+  --returns an ItemLink from id
+  return "|H1:item:" ..
+      cofferId .. ":0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h"
+end
+
+function Uncoffered.GetIdFromItemLink(itemLink) return GetItemLinkItemId(itemLink) end --returns Id from ItemLink
+
+function Uncoffered.GetMysteryFromNormal(itemLink)
+  --returns ItemLink of the Mystery Coffer, that the Normal Coffer is related to
+  local cofferId = Uncoffered.GetIdFromItemLink(itemLink)
+  --Finds the cofferId in Database and returns the cofferid it is saved in. Hardcoded Database!
+  for k, v in pairs(UncofferedData.CofferDbMini) do
+    for i = 1, #v do
+      if cofferId == v[i] then
+        return Uncoffered.GetItemLinkFromId(k)
+      end
+    end
+  end
+  return nil
+end
+
+function Uncoffered.GetNormalInfo(itemLink, type)
+  --returns info on a normal coffer
+  --type == 0 -> Undaunted Coffer
+  --type == 1 --> Imperial City Coffer
+
+  local cofferId = Uncoffered.GetIdFromItemLink(itemLink)
+  local cofferName = GetItemLinkName(itemLink)
+
+  --Ids of the sets that can drop from the coffer
+  --IC coffers only have 1 set, so every set2 var will be 0 or nil!
+  local _, _, _, _, _, set1Id = GetItemLinkContainerSetInfo(itemLink, 1)
+  local _, _, _, _, _, set2Id = GetItemLinkContainerSetInfo(itemLink, 2)
+
+  local set1Collected = 0
+  local set2Collected = 0
+  local total --IC coffer drop only 1 set -> Undaunted Coffers drop 6 shoulder pieces. IC Coffers drop 3.
+  if type == 0 then total = 6 end
+  if type == 1 then total = 3 end
+
+  for i = 4, 6 do --1 to 3 are the mask pieces. We need only the shoulders
+    if Uncoffered.IsCollectedFromSetId(set1Id, i) then
+      set1Collected = set1Collected + 1
+    end
+    if Uncoffered.IsCollectedFromSetId(set2Id, i) then
+      set2Collected = set2Collected + 1
+    end
+  end
+
+  local totalCollected = set1Collected + set2Collected
+  local pCollected = (totalCollected / total)
+  local pUncollected = 1 - pCollected
+
+  --returns all the collected info
+  return cofferId, cofferName, totalCollected, total, pCollected, pUncollected, set1Id, set2Id
+end
+
+function Uncoffered.GetMysteryInfo(itemLink, type)
+  --returns info on a mystery coffer
+  --type == 0 -> Undaunted Coffer
+  --type == 1 --> Imperial City Coffer
+
+  local cofferId = Uncoffered.GetIdFromItemLink(itemLink)
+  local cofferName = GetItemLinkName(itemLink)
+  local totalCollected = 0
+  --cofferAmount comes from hardcoded Database
+  local cofferAmount = #UncofferedData.CofferDbMini[cofferId]
+  local total
+  --Undaunted Coffers drop double the shoulder
+  if type == 0 then total = (cofferAmount * 3 * 2) end
+  if type == 1 then total = (cofferAmount * 3) end
+
+  --Table for saving the % of each normal coffer. Used to find the one with the best and output in the tooltip as advice, which one to open.
+  local normalPTable = {}
+
+  for i = 1, cofferAmount do
+    --Counts the collected amount of shoulder and saves the highest amount of uncollected % in the table
+    local NormalId = UncofferedData.CofferDbMini[cofferId][i]
+    local NormalItemLink = (Uncoffered.GetItemLinkFromId(NormalId))
+    local _, _, tCollected, _, _, pUncollected = Uncoffered.GetNormalInfo(NormalItemLink, type)
+    totalCollected = totalCollected + tCollected
+    table.insert(normalPTable, pUncollected)
+  end
+  local pCollected = (totalCollected / total)
+  local pUncollected = 1 - pCollected
+  local pCollectedPow = pCollected ^ 5
+  if type == 0 then pCollectedPow = pCollected ^ 5 end
+  if type == 1 then pCollectedPow = pCollected ^ 2 end
+  local pUncollectedPow = 1 - pCollectedPow
+
+  --returns all the collected info
+  return cofferId, cofferName, totalCollected, total, pCollected, pUncollected, pCollectedPow, pUncollectedPow
+      , normalPTable
+
+end
+
+function Uncoffered.GetBestNormalFromMystery(itemLink, type)
+  --returns the normal coffer, with the highest chances for a new item, thats related to the mystery coffer
+  --type == 0 -> Undaunted Coffer
+  --type == 1 --> Imperial City Coffer
+
+  --gets the table with the % of every related normal coffer
+  local cofferId, _, _, _, _, _, _, _, normalPTable = Uncoffered.GetMysteryInfo(itemLink, type)
+
+  local highestP = 0
+  local highestName = ""
+  local highestId
+  local highestItemLink
+  --finds the highest % and the name of the coffer (Same Order in the hardcoded Database)
+  for i = 1, #normalPTable do
+    if highestP < normalPTable[i] then
+      highestP = normalPTable[i]
+      highestId = UncofferedData.CofferDbMini[cofferId][i]
+      highestItemLink = Uncoffered.GetItemLinkFromId(highestId)
+      highestName = GetItemLinkName(highestItemLink)
+    end
+  end
+
+  --returns the highest % and the name of the coffer
+  return highestItemLink, highestId, highestName, highestP
+end
+
+local function GetToolTipTextMystery(itemLink, type)
+  --writes and returns string with the tooltip text for a mystery coffer
+  --type == 0 -> Undaunted Coffer
+  --type == 1 --> Imperial City Coffer
+
+  --get all the info needed for the tooltip
+  local _, cofferName, totalCollected, total, _, _, _, pUncollectedPow = Uncoffered.GetMysteryInfo(itemLink, type)
+  local _, _, highestName, highestP = Uncoffered.GetBestNormalFromMystery(itemLink, type)
+
+  local str = ""
+
+  --sets the vars for the difference between IC and Undaunted Coffers
+  local toOpen, normalCost
+  if type == 0 then
+    toOpen, normalCost = "5", "5|t24:24:esoui/art/currency/undauntedkey_64.dds|t"
+  end
+  if type == 1 then
+    toOpen, normalCost = "2", "20k|t24:24:esoui/art/currency/currency_telvar_64.dds|t"
+  end
+
+  --If everything is collected, we don't need any fancy tooltip
+  if totalCollected == total then
+    return string.format("%s/%s Collected.\nEverything has been collected. Well done!", totalCollected, total)
+  end
+
+  --concats the string
+  str = str .. string.format("%d/%d Collected\n", totalCollected, total)
+  str = str ..
+      string.format("Open %dx %s: %s\n", toOpen, cofferName, pColoredStr(pUncollectedPow, highestP, "00FF00", "FF0000"))
+  str = str ..
+      string.format("Best %s Coffer: %s (%s)", normalCost, highestName,
+        pColoredStr(highestP, pUncollectedPow, "00FF00", "FF0000"))
+  return str --returns the finished string
+end
+
+local function GetToolTipTextNormal(itemLink, type)
+  --writes and returns string with the tooltip text for a normal coffer
+  --type == 0 -> Undaunted Coffer
+  --type == 1 --> Imperial City Coffer
+
+  --get all the info needed for the tooltip
+  local _, cofferName, totalCollected, total, _, pUncollected = Uncoffered.GetNormalInfo(itemLink, type)
+  local _, mysteryName, _, _, _, _, _, pMysteryPow = Uncoffered.GetMysteryInfo(Uncoffered.GetMysteryFromNormal(itemLink)
+    , type)
+
+  --writes and returns string with the tooltip text for a normal coffer
+  local str = ""
+  local toOpen
+
+  --sets the vars for the difference between IC and Undaunted Coffers
+  if type == 1 then toOpen = "2" end
+  if type == 0 then toOpen = "5" end
+
+  --If everything is collected, we don't need any fancy tooltip
+  if totalCollected == total then
+    return string.format("%s/%s Collected.\nEverything has been collected. Well done!", totalCollected, total)
+  end
+
+  --concats the string
+  str = str .. string.format("%d/%d Collected\n", totalCollected, total)
+  str = str .. string.format("Open 1x %s: %s\n", cofferName, pColoredStr(pUncollected, pMysteryPow, "00FF00", "FF0000"))
+  str = str ..
+      string.format("Open %sx %s: %s", toOpen, mysteryName, pColoredStr(pMysteryPow, pUncollected, "00FF00", "FF0000"))
+
+  return str --returns the finished string
+end
+
+local function GetInfoText(itemLink)
+  --returns final tooltip text. Whatever string is returned here, will end up in the tooltip
+
+  if debug then return Uncoffered.GetIdFromItemLink(itemLink) end --Debug tooltip which shows the id of the item
+
+  local cofferId = Uncoffered.GetIdFromItemLink(itemLink)
+  local type
+
+  if UncofferedData.CofferDbMini[cofferId] ~= nil then
+    --Mystery Coffer
+    if cofferId == 184208 then type = 1 else type = 0 end --IC or Undaunted Coffer
+    return GetToolTipTextMystery(itemLink, type) --gets the tooltip string and returns it
+  end
+
+  if Uncoffered.GetMysteryFromNormal(itemLink) ~= nil then
+    --Normal Coffer
+    local mysteryCoffer = Uncoffered.GetMysteryFromNormal(itemLink)
+    if Uncoffered.GetIdFromItemLink(mysteryCoffer) == 184208 then type = 1 else type = 0 end --IC or Undaunted Coffer
+    return GetToolTipTextNormal(itemLink, type) --gets the tooltip string and returns it
+  end
+
+end
+
+local function AddInfo(tooltip, item)
+  --don't mess with this. Stolen, cause tooltips are hard
+  if item then
+    tooltip:AddVerticalPadding(8)
+    tooltip:AddLine(item, "", 1, 1, 1, CENTER, MODIFY_TEXT_TYPE_NONE, TEXT_ALIGN_CENTER, true)
+  end
+end
+
+local function TooltipHook(tooltipControl, method, linkFunc)
+  --don't mess with this. Stolen, cause tooltips are hard
+  local origMethod = tooltipControl[method]
+
+  tooltipControl[method] = function(self, ...)
+    origMethod(self, ...)
+    AddInfo(self, GetInfoText(linkFunc(...)))
+  end
+end
+
+local function ReturnItemLink(itemLink)
+  --don't mess with this. Stolen, cause tooltips are hard
+  return itemLink
+end
+
+---------------------------------------------
+-- OnAddOnLoaded --
+---------------------------------------------
+
+function Uncoffered.OnAddOnLoaded(event, addonName) --initialize the addon
+  if addonName ~= Uncoffered.name then return end
+
+  --don't mess with this. Stolen, cause tooltips are hard
+  TooltipHook(ItemTooltip, "SetStoreItem", GetStoreItemLink)
+  TooltipHook(ItemTooltip, "SetBagItem", GetItemLink)
+  TooltipHook(ItemTooltip, "SetTradeItem", GetTradeItemLink)
+  TooltipHook(ItemTooltip, "SetBuybackItem", GetBuybackItemLink)
+  TooltipHook(ItemTooltip, "SetAttachedMailItem", GetAttachedItemLink)
+  TooltipHook(ItemTooltip, "SetLootItem", GetLootItemLink)
+  TooltipHook(ItemTooltip, "SetTradingHouseItem", GetTradingHouseSearchResultItemLink)
+  TooltipHook(ItemTooltip, "SetTradingHouseListing", GetTradingHouseListingItemLink)
+  TooltipHook(ItemTooltip, "SetLink", ReturnItemLink)
+
+  TooltipHook(PopupTooltip, "SetLink", ReturnItemLink)
+
+
+
+  --Saved Settings (None currently)
+
+  -------------------------------------------
+  -- Settings --
+  -------------------------------------------
+  local panelData = {
+    type = "panel",
+    name = "Uncoffered",
+    author = '@Complicative',
+    version = Uncoffered.version,
+    website = "https://github.com/Complicative/Uncoffered",
+  }
+
+  LAM2:RegisterAddonPanel("UncofferedOptions", panelData)
+
+  local optionsData = {}
+  optionsData[#optionsData + 1] = {
+    type = "description",
+    text = "Adds a tooltip info to Shoulder Coffers (Undaunted and Imperical City) with info about the chances for a new shoulder pieces and which coffer to open for highest chances."
+  }
+  optionsData[#optionsData + 1] = {
+    type = "divider",
+  }
+  optionsData[#optionsData + 1] = {
+    type = "checkbox",
+    name = "Debug",
+    tooltip = "I wouldn't touch that",
+    getFunc = function() return debug end,
+    setFunc = function(value) debug = value end,
+  }
+
+  LAM2:RegisterOptionControls("UncofferedOptions", optionsData)
+end
+
+----------------------------------------------
+-- Events Setup --
+----------------------------------------------
+EVENT_MANAGER:RegisterForEvent(Uncoffered.name, EVENT_ADD_ON_LOADED, Uncoffered.OnAddOnLoaded)
+
+----------------------------------------------
+-- Slash Commands --
+----------------------------------------------
+
+local function printCoffers(id)
+  for i = 1, #UncofferedData.CofferDbMini[id] do
+    d(getTimeStamp() .. Uncoffered.GetItemLinkFromId(UncofferedData.CofferDbMini[id][i]))
+  end
+end
+
+SLASH_COMMANDS["/uncoffered"] = function(args)
+
+  if args == "debug" then debug = not debug
+    d(getTimeStamp() .. "debug has been set to " .. tostring(debug))
+    return
+  end
+  if args == "print" then
+    d(getTimeStamp() .. "---------------------------")
+    d("This is a debug function. I left it in, with debug off, since I think, it could be usefull in non-debug mode as well.")
+    d(getTimeStamp() .. "---------------------------")
+    d(getTimeStamp() .. Uncoffered.GetItemLinkFromId(153513))
+    printCoffers(153513)
+    d(getTimeStamp() .. "---------------------------")
+    d(getTimeStamp() .. Uncoffered.GetItemLinkFromId(153514))
+    printCoffers(153514)
+    d(getTimeStamp() .. "---------------------------")
+    d(getTimeStamp() .. Uncoffered.GetItemLinkFromId(153515))
+    printCoffers(153515)
+    d(getTimeStamp() .. "---------------------------")
+    d(getTimeStamp() .. Uncoffered.GetItemLinkFromId(184208))
+    printCoffers(184208)
+    d(getTimeStamp() .. "---------------------------")
+  end
+
+end
---
+++ Uncoffered/UncofferedData.lua
@@ -0,0 +1,473 @@
+UncofferedData = {}
+
+UncofferedData.CofferDbMini = {
+
+    [153513] = --Maj's Mystery Coffer
+    {
+        153512, --Banished Cells Coffer
+        153516, --Darkshade Caverns Coffer
+        153517, --Elden Hollow Coffer
+        153518, --Fungal Grotto Coffer
+        153519, --Spindleclutch Coffer
+        153520, --Wayrest Sewers Coffer
+    },
+    [153514] = --Gliron's Mystery Coffer
+    {
+        153521, --City of Ash Coffer
+        153522, --Crypt of Hearts Coffer
+        153524, --Frigid Crucible Coffer
+        153526, --Sands and Madness Coffer
+        153523, --Serpents and Sailors Coffer
+        153525, --Winds and Webs Coffer
+    },
+    [153515] = --Urgarlag's Mystery Coffer
+    {
+        167243, --Stonethorn Coffer
+        171714, --Flames of Ambition Coffer
+        159621, --Harrowstorm Coffer
+        181544, --Waking Flame Coffer
+        153527, --Imperial City Coffer
+        153528, --Shadows of the Hist Coffer
+        153529, --Horns of the Reach Coffer
+        153530, --Dragon Bones Coffer
+        184251, --Ascending Tide Coffer
+        153532, --Wrathstone Coffer
+        153533, --Scalebreaker Coffer
+        153531, --Wolfhunter Coffer
+    },
+    [184208] = --Imperial City Mystery Coffer
+    {
+        184118, --Baron Thirsk
+        175803, --Glorgoloch the Destroyer
+        175804, --Immolator Charr
+        184117, --Lady Malygda
+        184116, --Nunatak
+        175913, --Toal the Ever-Wakeful
+    }
+}
+
+-----------------------------
+-- Data Dump --
+-----------------------------
+--[[
+UncofferedData.Setsdb = {
+
+    [265] = "Shadowrend",
+    [170] = "Maw of the Infernal",
+
+    [268] = "Sentinel of Rkugamz",
+    [166] = "Engine Guardian",
+
+    [269] = "Chokethorn",
+    [167] = "Nightflame",
+
+    [266] = "Kra'gh",
+    [162] = "Spawn of Mephala",
+
+    [267] = "Swarm Mother",
+    [163] = "Bloodspawn",
+
+    [270] = "Slimecraw",
+    [165] = "Scourge Harvester",
+
+    ------------------------------
+
+    [272] = "Infernal Guardian",
+    [169] = "Valkyn Skoria",
+
+    [273] = "Ilambris",
+    [168] = "Nerien'eth",
+
+    [278] = "The Troll King",
+    [274] = "Iceheart",
+
+    [280] = "Grothdarr",
+    [276] = "Tremorscale",
+
+    [271] = "Sellistrix",
+    [277] = "Pirate Skeleton",
+
+    [279] = "Selene",
+    [275] = "Stormfist",
+
+    ------------------------------
+
+    [632] = "Kargaeda",
+    [633] = "Nazaray",
+
+    [349] = "Thurvokun",
+    [350] = "Zaan",
+
+    [577] = "Encratis's Behemoth",
+    [578] = "Baron Zaudrus",
+
+    [478] = "Mother Ciannait",
+    [479] = "Kjalnar's Nightmare",
+
+    [341] = "Earthgore",
+    [342] = "Domihaus",
+
+    [164] = "Lord Warden",
+    [183] = "Molag Kena",
+
+    [459] = "Maarselok",
+    [458] = "Grundwulf",
+
+    [256] = "Mighty Chudan",
+    [257] = "Velidreth",
+
+    [535] = "Lady Thorn",
+    [534] = "Stone Husk",
+
+    [608] = "Prior Thierric",
+    [609] = "Magma Incarnate",
+
+    [398] = "Vykosa",
+    [397] = "Balorgh",
+
+    [436] = "Symphony of Blades",
+    [432] = "Stonekeeper",
+
+    ------------------------------
+
+    [600] = "Glorgoloch the Destroyer",
+    [599] = "Immolator Charr",
+    [598] = "Zoal the Ever-Wakeful",
+    [634] = "Nunatak",
+    [635] = "Lady Malygda",
+    [636] = "Baron Thirsk",
+
+}
+
+UncofferedData.Cofferdb = {
+    [171714] =
+    {
+        [1] = 577,
+        [2] = 578,
+        ["name"] = "Flames of Ambition Coffer",
+    },
+    [159621] =
+    {
+        [1] = 478,
+        [2] = 479,
+        ["name"] = "Harrowstorm Coffer",
+    },
+    [153512] =
+    {
+        [1] = 265,
+        [2] = 170,
+        ["name"] = "Banished Cells Coffer",
+    },
+    [181544] =
+    {
+        [1] = 608,
+        [2] = 609,
+        ["name"] = "Waking Flame Coffer",
+    },
+    [153513] =
+    {
+        [1] = 0,
+        [2] = 0,
+        ["name"] = "Maj's Mystery Coffer",
+    },
+    [153514] =
+    {
+        [1] = 0,
+        [2] = 0,
+        ["name"] = "Glirion's Mystery Coffer",
+    },
+    [167243] =
+    {
+        [1] = 534,
+        [2] = 535,
+        ["name"] = "Stonethorn Coffer",
+    },
+    [153516] =
+    {
+        [1] = 268,
+        [2] = 166,
+        ["name"] = "Darkshade Caverns Coffer",
+    },
+    [153517] =
+    {
+        [1] = 269,
+        [2] = 167,
+        ["name"] = "Elden Hollow Coffer",
+    },
+    [153518] =
+    {
+        [1] = 266,
+        [2] = 162,
+        ["name"] = "Fungal Grotto Coffer",
+    },
+    [153519] =
+    {
+        [1] = 267,
+        [2] = 163,
+        ["name"] = "Spindleclutch Coffer",
+    },
+    [153520] =
+    {
+        [1] = 270,
+        [2] = 165,
+        ["name"] = "Wayrest Sewers Coffer",
+    },
+    [153521] =
+    {
+        [1] = 272,
+        [2] = 169,
+        ["name"] = "City of Ash Coffer",
+    },
+    [153522] =
+    {
+        [1] = 273,
+        [2] = 168,
+        ["name"] = "Crypt of Hearts Coffer",
+    },
+    [153523] =
+    {
+        [1] = 271,
+        [2] = 277,
+        ["name"] = "Serpents and Sailors Coffer",
+    },
+    [153524] =
+    {
+        [1] = 278,
+        [2] = 274,
+        ["name"] = "Frigid Crucible Coffer",
+    },
+    [153525] =
+    {
+        [1] = 279,
+        [2] = 275,
+        ["name"] = "Winds and Webs Coffer",
+    },
+    [153526] =
+    {
+        [1] = 276,
+        [2] = 280,
+        ["name"] = "Sands and Madness Coffer",
+    },
+    [153527] =
+    {
+        [1] = 164,
+        [2] = 183,
+        ["name"] = "Imperial City Coffer",
+    },
+    [153528] =
+    {
+        [1] = 257,
+        [2] = 256,
+        ["name"] = "Shadows of the Hist Coffer",
+    },
+    [153529] =
+    {
+        [1] = 341,
+        [2] = 342,
+        ["name"] = "Horns of the Reach Coffer",
+    },
+    [153530] =
+    {
+        [1] = 349,
+        [2] = 350,
+        ["name"] = "Dragon Bones Coffer",
+    },
+    [184251] =
+    {
+        [1] = 633,
+        [2] = 632,
+        ["name"] = "Ascending Tide Coffer",
+    },
+    [153532] =
+    {
+        [1] = 436,
+        [2] = 432,
+        ["name"] = "Wrathstone Coffer",
+    },
+    [153533] =
+    {
+        [1] = 458,
+        [2] = 459,
+        ["name"] = "Scalebreaker Coffer",
+    },
+    [153515] =
+    {
+        [1] = 0,
+        [2] = 0,
+        ["name"] = "Urgarlag's Mystery Coffer",
+    },
+    [153531] =
+    {
+        [1] = 398,
+        [2] = 397,
+        ["name"] = "Wolfhunter Coffer",
+    },
+
+
+}
+
+UncofferedData.CofferdbSorted = {
+
+    [153513] =
+    {
+        [153512] =
+        {
+            [1] = 265,
+            [2] = 170,
+            ["name"] = "Banished Cells Coffer",
+        },
+        [153516] =
+        {
+            [1] = 268,
+            [2] = 166,
+            ["name"] = "Darkshade Caverns Coffer",
+        },
+        [153517] =
+        {
+            [1] = 269,
+            [2] = 167,
+            ["name"] = "Elden Hollow Coffer",
+        },
+        [153518] =
+        {
+            [1] = 266,
+            [2] = 162,
+            ["name"] = "Fungal Grotto Coffer",
+        },
+        [153519] =
+        {
+            [1] = 267,
+            [2] = 163,
+            ["name"] = "Spindleclutch Coffer",
+        },
+        [153520] =
+        {
+            [1] = 270,
+            [2] = 165,
+            ["name"] = "Wayrest Sewers Coffer",
+        },
+        ["name"] = "Maj's Mystery Coffer",
+
+    },
+    [153514] =
+    {
+        ["name"] = "Glirion's Mystery Coffer",
+        [153521] =
+        {
+            [1] = 272,
+            [2] = 169,
+            ["name"] = "City of Ash Coffer",
+        },
+        [153522] =
+        {
+            [1] = 273,
+            [2] = 168,
+            ["name"] = "Crypt of Hearts Coffer",
+        },
+        [153524] =
+        {
+            [1] = 278,
+            [2] = 274,
+            ["name"] = "Frigid Crucible Coffer",
+        },
+        [153526] =
+        {
+            [1] = 276,
+            [2] = 280,
+            ["name"] = "Sands and Madness Coffer",
+        },
+        [153523] =
+        {
+            [1] = 271,
+            [2] = 277,
+            ["name"] = "Serpents and Sailors Coffer",
+        },
+        [153525] =
+        {
+            [1] = 279,
+            [2] = 275,
+            ["name"] = "Winds and Webs Coffer",
+        },
+
+    },
+    [153515] =
+    {
+        [1] = 0,
+        [2] = 0,
+        ["name"] = "Urgarlag's Mystery Coffer",
+        [167243] =
+        {
+            [1] = 534,
+            [2] = 535,
+            ["name"] = "Stonethorn Coffer",
+        },
+        [171714] =
+        {
+            [1] = 577,
+            [2] = 578,
+            ["name"] = "Flames of Ambition Coffer",
+        },
+        [159621] =
+        {
+            [1] = 478,
+            [2] = 479,
+            ["name"] = "Harrowstorm Coffer",
+        },
+        [181544] =
+        {
+            [1] = 608,
+            [2] = 609,
+            ["name"] = "Waking Flame Coffer",
+        },
+        [153527] =
+        {
+            [1] = 164,
+            [2] = 183,
+            ["name"] = "Imperial City Coffer",
+        },
+        [153528] =
+        {
+            [1] = 257,
+            [2] = 256,
+            ["name"] = "Shadows of the Hist Coffer",
+        },
+        [153529] =
+        {
+            [1] = 341,
+            [2] = 342,
+            ["name"] = "Horns of the Reach Coffer",
+        },
+        [153530] =
+        {
+            [1] = 349,
+            [2] = 350,
+            ["name"] = "Dragon Bones Coffer",
+        },
+        [184251] =
+        {
+            [1] = 633,
+            [2] = 632,
+            ["name"] = "Ascending Tide Coffer",
+        },
+        [153532] =
+        {
+            [1] = 436,
+            [2] = 432,
+            ["name"] = "Wrathstone Coffer",
+        },
+        [153533] =
+        {
+            [1] = 458,
+            [2] = 459,
+            ["name"] = "Scalebreaker Coffer",
+        },
+
+        [153531] =
+        {
+            [1] = 398,
+            [2] = 397,
+            ["name"] = "Wolfhunter Coffer",
+        },
+    },
+}
+]] --
